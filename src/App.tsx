cd7dfa6730e033d60e7b7bfad1ec3f0a9d94c397
Vidit Shah <vidit0210@protonmail.com>
2023-05-08 19:40:25 +0530

    Update in Sign Up

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import SignInForm from "./components/signIN";
+// import SignInForm from "./components/signIN";
+import SignupForm from "./components/signUP";
 import { AuthProvider } from "./context/AuthContext";
 function App() {
   return (
     <>
       <AuthProvider>
-        <SignInForm />
+        <SignupForm />
       </AuthProvider>
     </>
   );
