5d79f3fef3770b41d66105ad777c394ffe8f77de
Vidit Shah <vidit0210@protonmail.com>
2023-05-10 19:53:36 +0530

    TodoForm Skeleton

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,9 +1,11 @@
 import NavBar from "../../components/NavBar";
+import TodoForm from "../../components/TodoForm";
 
 function TodoPage() {
   return (
     <div>
       <NavBar />
+      <TodoForm />
     </div>
   );
 }
